09ca15c44dd1e8e6208d9de48be6382154f2f1a5
didiervuillet <didiervuillet@hotmail.com>
2023-07-14 16:33:09 -0300

    helloworld 1

---
+++ back/main.java
@@ -0,0 +1,43 @@
+package src.back;
+
+import java.awt.BorderLayout;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.Insets;
+import java.awt.TextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+import javax.swing.JButton;
+import javax.swing.JFormattedTextField;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTabbedPane;
+import javax.swing.JTextField;
+
+import src.Front.Identification;
+
+public class main {
+
+static int b=0;
+
+static Identification I= new Identification();
+
+
+public static void main(String[] args) {
+		I.bienvenue();}
+
+
+
+}
