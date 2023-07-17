9c89596429b3d697f46fedcb3bc69cd846d824e2
didiervuillet <didiervuillet@hotmail.com>
2023-07-17 17:49:13 -0300

    guten morgen

--- src/helloworld/world.java
+++ src/helloworld/world.java
@@ -6,7 +6,7 @@ public class world {
 		// TODO Auto-generated method stub
 		// Bonjour Jean
 		// Bonjour Didier
-		System.out.println("Bonjour à tout le monde bom dia good morning");
+		System.out.println("Bonjour à tout le monde bom dia good morning guten morgen");
 	}
 
 }
