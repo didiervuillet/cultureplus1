062283c342c558ca8f920a17af7d1ef9a6c1e480
didiervuillet <didiervuillet@hotmail.com>
2023-07-17 17:38:04 -0300

    ok

---
+++ src/helloworld/world.java
@@ -0,0 +1,12 @@
+package helloworld;
+
+public class world {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Bonjour Jean
+		// Bonjour Didier
+		System.out.println("Bonjour à tout le monde bom dia");
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author didiervuillet
+ *
+ */
+module helloworld {
+}
